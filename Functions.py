5bb6af8752632bad4623f615269991791b9ff67f
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-07 19:51:03 +0500

    updated
    add code to function.py

--- Functions.py
+++ Functions.py
@@ -6,13 +6,24 @@
     Ustoz: Anvar Narzullayev
 
 """
-def hello_world(name):
 
-    print(f"Assalomu aleykum {name.title()}")
 
-hello_world("Hasan")
+# def hello_world(name):
+#
+#     print(f"Assalomu aleykum {name.title()}")
+#
+# hello_world("Hasan")
+#
+# hello_world("Husan")
+#
+# hello_world("Akbar")
 
-hello_world("Husan")
+def toliq_ism_yasa(ism="anvar", familiya="narzullayev"):
+    toliq_ism = F"{ism.title()} {familiya.title()}"
 
-hello_world("Akbar")
+    return toliq_ism
 
+
+talaba = toliq_ism_yasa('anvar', 'narzullayev')
+
+print(talaba)
