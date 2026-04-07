e663c584c1e520a41a7a2636c1dbc332e75ee453
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-07 13:22:59 +0500

    updated and add file
    add function.py file

---
+++ Functions.py
@@ -0,0 +1,18 @@
+"""
+    Sana 07:05:26 yil
+
+    Mavzu: Funksiyalar
+
+    Ustoz: Anvar Narzullayev
+
+"""
+def hello_world(name):
+
+    print(f"Assalomu aleykum {name.title()}")
+
+hello_world("Hasan")
+
+hello_world("Husan")
+
+hello_world("Akbar")
+
