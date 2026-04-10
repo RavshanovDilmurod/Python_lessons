5a2f4247cb561c094090c16b4728bb56d8ccaf7d
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-10 19:32:48 +0500

    update if_else.py file

--- if_else.py
+++ if_else.py
@@ -7,38 +7,38 @@ Mavzu: If-Else Shartli tarmoqlanish
 
 """
 
-# cars = ["bmw", "volsvagen", "mers", "damas", "neksiya"]
+cars = ["bmw", "volsvagen", "mers", "damas", "neksiya"]
 
-# for avto in cars:
-    
-    
-#     if avto=="bmw":
-    
-#         print(avto.upper())
-    
-#     else:
-    
-#         print(avto.title())
-# ism = input("Ismingizni kiriting:")
+for avto in cars:
 
-# if ism.lower() != "ali":
-
-
-#        print(f"Uzr, {ism.title()} biz Alini kutyapmiz: ")
-
-# else:
-
-#     print("Assalomu aleykum Ali:")
-
-# javob = float(input("12*6 nechichi bo'ladi ? >>>"))
-
-# if javob !=72:
     
-#     print("Javob xato")
-    
-# else:
-    
-#     print("Javob to'g'ri:")
+    if avto=="bmw":
+
+        print(avto.upper())
+
+    else:
+
+        print(avto.title())
+ism = input("Ismingizni kiriting:")
+
+if ism.lower() != "ali":
+
+
+       print(f"Uzr, {ism.title()} biz Alini kutyapmiz: ")
+
+else:
+
+    print("Assalomu aleykum Ali:")
+
+javob = float(input("12*6 nechichi bo'ladi ? >>>"))
+
+if javob !=72:
+
+    print("Javob xato")
+
+else:
+
+    print("Javob to'g'ri:")
 
 yosh = int(input("yoshingizni kiriting: "))
 
