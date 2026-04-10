109693821216d6b6269809d5bca44e20f1d1c5bd
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-10 19:30:11 +0500

    updated

--- return_price_from_function.py
+++ return_price_from_function.py
@@ -1,4 +1,4 @@
-def toliq_ism_yasa(ism="anvar", familiya="narzullayev"):
+n def toliq_ism_yasa(ism="anvar", familiya="narzullayev"):
     toliq_ism = F"{ism.title()} {familiya.title()}"
 
     return toliq_ism
