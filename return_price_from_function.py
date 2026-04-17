2ddc7af427c07d360594ebbaeeaa295ba8689b18
RavshanovDilmurod <shadowfight257064948@gmail.com>
2026-04-17 09:28:29 +0500

    updated

--- return_price_from_function.py
+++ return_price_from_function.py
@@ -1,12 +1,32 @@
-def toliq_ism_yasa(ism="anvar", familiya="narzullayev"):
+"""
 
-    toliq_ism = F"{ism.title()} {familiya.title()}"
 
-    return toliq_ism
 
-talaba = toliq_ism_yasa('anvar', 'narzullayev')
+"""
+#
+# def toliq_ism_yasa(ism="anvar", familiya="narzullayev"):
+#
+#     toliq_ism = F"{ism.title()} {familiya.title()}"
+#
+#     return toliq_ism
+#
+# talaba = toliq_ism_yasa('anvar', 'narzullayev')
+#
+# print(talaba)
 
-print(talaba)
+# def ism_familiya(name, surname, birth):
+#     """ Foydlaanuvchi ismi va familiyasini qabul qilib chiqaruvchi funksiya """
+#
+#     print(f"Foydalanuvchi Ismi: {name.title()}\n"
+#           f"Foydalanuvchi Familiyasi: {surname.title()}\n"
+#           f"Foydalanuvchi yoshi: {2026 - birth}\n")
+#
+# ism_familiya("dilmurod", "ravshanov", birth = 2005)
 
+def cal_age(birth_year, now_year = 2026):
+
+    print(f"Yoshingiz {now_year - birth_year} ekan:")
+
+cal_age(birth_year = 2005)
 
 
